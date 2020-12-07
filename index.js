a7ea2d01e1a9c76223cd8e95ae570fd60604e839
hjban <baneju212@gmail.com>
2020-12-07 17:11:22 +0900

    로그인 기능 생성

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const { User } = require("./models/User");
 
+const mongoose = require("mongoose");
 const config = require("./config/key");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(cors());
 
-const mongoose = require("mongoose");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose
 	.connect(config.mongoURI, {
@@ -26,20 +30,41 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+// 회원가입 할 때 필요한 정보들을 cilent 에서 가져오면 그것들을 db에 넣는다.
 app.post("/register", (req, res) => {
-	// 회원가입 할 때 필요한 정보들을 cilent 에서 가져오면 그것들을 db에 넣는다.
-
 	const user = new User(req.body);
 
-	user.save((err, userInfo) => {
+	user.save((err, doc) => {
 		if (err) return res.json({ success: false, err });
-
 		return res.status(200).json({
 			success: true,
 		});
 	});
 });
 
+// 요청된 이메일과 비밀번호 확인
+app.post("/login", (req, res) => {
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (!user)
+			return res.json({
+				loginSuccess: false,
+				message: "Auth failed, email not found",
+			});
+
+		user.checkPassword(req.body.password, (err, isMatch) => {
+			if (!isMatch) return res.json({ loginSuccess: false, message: "Wrong password" });
+
+			user.generateToken((err, user) => {
+				if (err) return res.status(400).send(err);
+				res.cookie("w_authExp", user.tokenExp);
+				res.cookie("w_auth", user.token).status(200).json({
+					loginSuccess: true,
+					userId: user._id,
+				});
+			});
+		});
+	});
+});
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
